64e8bb6780668912e07d61b1ae837b9ba452c524
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2022-02-01 18:51:20 +0100

    created interfaces and props

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -5,3 +5,25 @@ import (
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+type User struct {
+    gorm.Model
+    Username string
+    Email string
+    Password string
+}
+
+type Account struct {
+    gorm.Model
+    Type string
+    Name string
+    Balance uint
+    UserID uint
+}
+
+func connectDB() *gorm.DB {
+    db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=BankiePostgresApp password=b4nk1eg0 sslmode=disable")
+    helpers.HandleErr(err)
+    return db
+}
+
