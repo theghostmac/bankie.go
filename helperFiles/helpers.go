68f92c4dc2779d7a9a9859ff0c590386f922f31c
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2022-02-01 18:45:04 +0100

    hashing/salting function and migrations.go file

--- helperFiles/helpers.go
+++ helperFiles/helpers.go
@@ -11,3 +11,9 @@ func HandleErr(err error) {
 	}
 }
 
+func HashAndSalt(pass []byte) string {
+    hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+    HandleErr(err)
+
+    return string(hashed)
+}
---
+++ migrations/migrations.go
@@ -0,0 +1,7 @@
+package migrations
+
+import (
+	"github.com/theghostmac/bankie.go/helperFiles"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+)
