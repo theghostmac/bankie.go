032c765aba06d01109af64cd609c62d07dff90cf
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2022-02-04 22:42:18 +0100

    refactored interfaces as accoint data store

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -3,6 +3,7 @@ package migrations
 import (
 	"github.com/theghostmac/bankie.go/helperFiles"
     "github.com/jinzhu/gorm"
+    "github.com/theghostmac/bankie.go/interfaces"
     _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
@@ -24,13 +25,15 @@ type Account struct {
 func connectDB() *gorm.DB {
     db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=BankiePostgresApp password=b4nk1eg0 sslmode=disable")
     helpers.HandleErr(err)
-    return db 
+    return db
+
+
 }
 
 func createAccounts() {
     db := connectDB()
 
-    users := [4]User{
+    users := &[4]interfaces.User{
         {Username: "MacBobby", Email: "macbobbychibuzor@gmail.com"},
         {Username: "GhostMac", Email: "theghostmac@gmail.com"},
         {Username: "Chibuzor", Email: "peekube01@gmail.com"},
@@ -39,16 +42,18 @@ func createAccounts() {
 
     for i := 0; i < len(users); i++ {
         generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
-        user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
+        user := &interfaces.User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
         db.Create(&user)
 
-        account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
+        account := &interfaces.Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
         db.Create(&account)
     }
     defer db.Close()
 }
 
 func Migrate() {
+    users = &interfaces.User{}
+    account = &interfaces.Account{}
     db := connectDB()
     db.AutoMigrate(&User{}, &Account{})
     defer db.Close()
