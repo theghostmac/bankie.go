db30cef2ec69f0de7c091ea89779461431014b5b
Ghost Mac <theghostmac@gmail.com>
2022-02-11 01:42:10 +0100

    created users.go file

---
+++ users.go
@@ -0,0 +1,14 @@
+package users
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/theghostmac/bankie.go/helperFiles"
+	"github.com/theghostmac/bankie.go/interfaces"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(username string, pass string) map[string]interface{} {
+
+}
