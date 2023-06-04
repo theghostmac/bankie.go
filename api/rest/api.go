ffbcf0592b392e0d45fc000ac90c32501057bb1a
theghostmac <theghostmac@gmail.com>
2023-06-04 22:22:35 +0100

    commented code for api.go

--- api/rest/api.go
+++ api/rest/api.go
@@ -9,10 +9,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// APIServer creates a port for the endpoint.
 type APIServer struct {
 	listenToPort string
 }
 
+// APIFunc is a one-off definition of the HTTP handler declaration for each function.
 type APIFunc func(http.ResponseWriter, *http.Request) error
 
 // HTTPHandleFunc decorates the APIFunc into an HTTP handler.
@@ -24,6 +26,7 @@ func HTTPHandleFunc(f APIFunc) http.HandlerFunc {
 	}
 }
 
+// APIError handles error messages for all API methods.
 type APIError struct {
 	Error string
 }
